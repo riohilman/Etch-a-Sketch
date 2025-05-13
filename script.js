cbad775367fad31787f4ea98aa6b268066db6486
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-13 07:41:10 +0700

    Create 16x16 grid of square div

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 256; i++) {
+    const grid = document.createElement("div");
+    grid.setAttribute("class", "box");
+    
+    container.appendChild(grid);
+}
+
