b9cc521c438651a0758373bd83e43ae40bb55d52
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-15 09:44:15 +0700

    Add button to change grid

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#change-grid");
+
 
 for (let i = 0; i < 256; i++) {
     const grid = document.createElement("div");
     grid.setAttribute("class", "box");
-    
+    grid.style.width = `${600 / 16}px`;
+    grid.style.height = `${600 / 16}px`;
     container.appendChild(grid);
 
     grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "red";
+        grid.style.backgroundColor = "black";
     })
-}
+}   
+
+button.addEventListener("click", () => {
+    let side;
+    do {
+        side = parseInt(prompt("How many squares per side? (1–100)"));
+    } while (isNaN(side) || side < 1 || side > 100);
+    
+    container.innerHTML = "";
+    const boxSize = 600 / side;
+    for (let i = 0; i < side*side; i++) {
+        const grid = document.createElement("div");
+        grid.setAttribute("class", "box");
+        grid.style.width = `${boxSize}px`;
+        grid.style.height = `${boxSize}px`;
+        container.appendChild(grid);
+    
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "black";
+        })
+    }        
+});
+
+
+
+
 
